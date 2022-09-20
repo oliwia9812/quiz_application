71b438e81251989c5102b581177d2bc0154d4dcd
Oliwia <Oliwia.9812@gmail.com>
2022-09-20 09:14:29 +0200

    add logic to check correct answer

--- lib/utilities/widgets/answerButton.dart
+++ lib/utilities/widgets/answerButton.dart
@@ -7,51 +7,67 @@ enum ButtonState {
 }
 
 class AnswerButton extends StatefulWidget {
-  String answerText;
-  AnswerButton(this.answerText, {Key? key}) : super(key: key);
+  final String answerText;
+  final String correctAnswer;
+  const AnswerButton(
+      {Key? key, required this.answerText, required this.correctAnswer})
+      : super(key: key);
 
   @override
   State<AnswerButton> createState() => _AnswerButtonState();
 }
 
 class _AnswerButtonState extends State<AnswerButton> {
-  late ButtonState buttonState = ButtonState.noAnswer;
+  ButtonState buttonState = ButtonState.noAnswer;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         setState(() {
-          buttonState = ButtonState.correctAnswer;
+          if (widget.answerText == widget.correctAnswer) {
+            buttonState = ButtonState.correctAnswer;
+          } else if (widget.answerText != widget.correctAnswer) {
+            buttonState = ButtonState.incorrectAnswer;
+          }
         });
       },
       child: Container(
         margin: const EdgeInsets.only(bottom: 12.0),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(width: 1, color: _getBorderColor())),
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(
+            width: 1,
+            color: _getBorderColor(),
+          ),
+        ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                this.widget.answerText,
+                widget.answerText,
                 style: TextStyle(
-                    fontWeight: FontWeight.w500, color: Colors.grey.shade800),
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey.shade800,
+                ),
               ),
               Container(
-                height: 24,
-                width: 24,
+                height: 24.0,
+                width: 24.0,
                 decoration: BoxDecoration(
                   color: _getButtonColor(),
-                  border: Border.all(width: 1.4, color: _getBorderColor()),
+                  border: Border.all(
+                    width: 1.4,
+                    color: _getBorderColor(),
+                  ),
                   shape: BoxShape.circle,
                 ),
                 child: Icon(
                   _getIcon(),
                   color: Colors.white,
-                  size: 16,
+                  size: 16.0,
                 ),
               )
             ],
